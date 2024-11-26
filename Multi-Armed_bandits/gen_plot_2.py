8f225587ff0c1e9256d654094bf0ab50dcda1090
biancaraetchi <mold.academ@gmail.com>
2024-11-26 22:26:02 +0100

    partial graph; partial gradient bandit

---
+++ Multi-Armed_bandits/gen_plot_2.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from MA_Bandit import MultiArmedBandit
+from policies.epsilon_greedy import Epsilon_Greedy
+from policies.greedy import Greedy
+from policies.softmax import Softmax
+from policies.UCB import UCB
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+num_arms = 10
+config = [{'policy': Greedy, 'custom':'learning_rate'},
+          {'policy':Epsilon_Greedy, 'custom':'epsilon'},
+          {'policy':UCB, 'custom':'conf_level'},
+          {'policy':Softmax, 'custom':'temperature'}]
+x_values = [1/128, 1/64, 1/32, 1/16, 1/8, 1/4, 1/2, 1, 2, 4]
+
+# amount of timesteps per run
+t_steps = 1000
+
+plot1 = pd.DataFrame()
+df_rewards = pd.DataFrame()
+
+np.random.seed(42)
+
+for p in range(len(config)):
+    bandit_means = np.random.normal(loc= 0, scale= 1,size= num_arms).tolist()
+    bandit_stds = [1] * num_arms
+
+    bandit = MultiArmedBandit(num_arms, bandit_means, bandit_stds)
+    
+
+    final_plot = []
+
+    for x in x_values:
+        print({config[p].get('custom'):x})
+        policy = config[p].get('policy')(**{config[p].get('custom'):x, 'num_arms':num_arms}) 
+        reward_list = []
+        for t in range(t_steps):
+            action = policy.select_action()
+            reward = bandit.pull_arm(action)
+            policy.update(action,reward)
+
+            reward_list.append(reward)
+        final_plot.append(pd.DataFrame(reward_list).mean())
+    
+    
+    plt.figure()
+    plt.plot(x_values,final_plot)
+    plt.xlabel("Hyperparameter Value")
+    plt.ylabel("Average Reward")
+    plt.title("Evaluation of Exploration Methods on 10-Armed Bandit")
+    plt.legend()
+
+    plt.grid(True, linestyle="--", alpha=0.6)
+    plt.show()
--- Multi-Armed_bandits/policies/gradient_bandit.py
+++ Multi-Armed_bandits/policies/gradient_bandit.py
@@ -2,4 +2,40 @@
 Implementation of the Gradient bandit policy, AKA: Action preference with baseline.
 """
 
-# TODO
+"""
+Implementation of the epsilon-greedy policy
+"""
+
+from policies.abstract_policy import Policy
+import numpy as np
+
+class GradientBandit(Policy):
+    """
+    Gradient-bandit policy.
+    """
+    def __init__(self, epsilon: float, num_arms: int):
+        
+        self.epsilon = epsilon
+        self.num_arms = num_arms
+        self.q_values = np.zeros(num_arms)
+        self.arm_cnts = np.zeros(num_arms)
+
+    
+    def select_action(self) -> int:
+        # random value between 0 and 1
+        i = np.random.rand()
+
+        if i < self.epsilon:
+            # random selection: Exploration
+            action = np.random.choice(self.num_arms)
+        else:
+            # greedy selection: Exploitation
+            action = np.argmax(self.q_values)
+        
+        return action
+    
+    def update(self, arm: int, reward: float) -> None:
+        self.arm_cnts[arm] += 1
+
+        # update corresponding Q-value using Sample-average method.
+        self.q_values[arm] += (reward - self.q_values[arm]) / self.arm_cnts[arm]
